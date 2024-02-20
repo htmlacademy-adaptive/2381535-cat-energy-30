3ee158b2890b23ae2872f3728a170f22d82a21d3
Vasiliev Andrey <axc1995@mail.ru>
2024-02-20 08:41:31 +0300

    Первые 2 пункта из списка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+// Раскрывющийся список Меню
+const navToggle = document.querySelector('.main-header__button');
+const navMenu = document.querySelector('.main-header__navigation');
+
+navMenu.classList.remove('main-header__navigation--nojs');
+navToggle.classList.remove('main-header__button--active');
+
