f91ddaacb976e7337e24fe1b5d8db2b81a4f434f
Vasiliev Andrey <axc1995@mail.ru>
2024-02-28 20:15:08 +0300

    Контакты и карта для всех страниц

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,3 +5,10 @@ const navMenu = document.querySelector('.main-header__navigation');
 navMenu.classList.remove('main-header__navigation--nojs');
 navToggle.classList.remove('main-header__button--active');
 
+
+//  const contactsWrapper = document.querySelector('.contacts__wrapper');
+//  if (window.innerWidth >= 1280) {
+//    contactsWrapper.classList.remove('contacts__wrapper--background');
+//  } else if (window.innerWidth < 1280) {
+//    contactsWrapper.classList.add('contacts__wrapper--background');
+//  }
