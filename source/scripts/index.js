f9f143e2623405faa776730b0610a08a14f3b4ae
Vasiliev Andrey <axc1995@mail.ru>
2024-03-05 03:07:35 +0300

    Menu all pages

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,14 +1,13 @@
 // Раскрывющийся список Меню
-const navToggle = document.querySelector('.main-header__button');
+const navToggle = document.querySelector('.main-header__button--closed');
 const navMenu = document.querySelector('.main-header__navigation');
 
+navToggle.classList.remove('main-header__button--nojs');
 navMenu.classList.remove('main-header__navigation--nojs');
-navToggle.classList.remove('main-header__button--active');
 
-
-//  const contactsWrapper = document.querySelector('.contacts__wrapper');
-//  if (window.innerWidth >= 1280) {
-//    contactsWrapper.classList.remove('contacts__wrapper--background');
-//  } else if (window.innerWidth < 1280) {
-//    contactsWrapper.classList.add('contacts__wrapper--background');
-//  }
+navToggle.onclick = function() {
+  navMenu.classList.toggle('main-header__navigation--closed');
+  navMenu.classList.toggle('main-header__navigation--opened');
+  navToggle.classList.toggle('main-header__button--opened');
+  navToggle.classList.toggle('main-header__button--closed');
+};
